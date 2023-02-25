2fb739af0557b7447cf367dd511cd8d5ad95f45a
zidanmp <zidanmp786@gmail.com>
2023-02-26 01:57:23 +0530

    Model updated

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -76,8 +76,16 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        
+        }
     }
 }
 
@@ -120,7 +128,6 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    '/var/www/static/',
 ]
 
 # Default primary key field type
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    booking_id = models.IntegerField()
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateTimeField()
+    
+class Menu(models.Model):
+    menu_id = models.IntegerField()
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
